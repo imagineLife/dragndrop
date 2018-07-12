fc6f1e80bca27a5c904d142e2c06befae14ff67f
Eric Laursen <ericjlaursen@gmail.com>
2018-07-12 13:04:55 -0400

    Includes optional filled box & conditional class.

--- reacted/src/components/Box/index.js
+++ reacted/src/components/Box/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Box.css';
+import BoxContent from '../BoxContent'
 
 export default function Box(props){
-	return(
-		<div id={props.boxID} className="empty {props.filledClass}"></div>
-	)
+	let filled = (props.filledClass) ? props.filledClass : 'empty';
+
+	var classes = `${filled}`;
+	console.log('box props')
+	console.log(props)
+
+	if(props.boxFilled){
+		return(
+			<div id={props.boxID} className={classes}>
+				<BoxContent />
+			</div>
+		)
+	}else{
+		return(
+			<div id={props.boxID} className={classes}></div>
+		)
+	}
 }
