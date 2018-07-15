089ae7a8d42039f4717282dff7ded68ea2b34238
Eric Laursen <ericjlaursen@gmail.com>
2018-07-15 15:34:20 -0400

    Updates props & layout.

--- reacted/src/components/BoxContent/index.js
+++ reacted/src/components/BoxContent/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './BoxContent.css';
 
 export default function BoxContent(props){
+	console.log('inner box props')
 	return(
-		<div id="thisID" className="fill" draggable="true"></div>
+		<div 
+			id={props.filledID} 
+			onDragStart={props.dragStart} 
+			className="fill" 
+			draggable="true"
+			></div>
 	)
 }
